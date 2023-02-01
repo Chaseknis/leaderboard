96f14bce500e4e6b537dea208c10efcd90498ce6
chaseknis <chase6th@gmail.com>
2023-02-01 12:09:36 +0200

    make changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
 
 module.exports = {
   mode: 'development',
