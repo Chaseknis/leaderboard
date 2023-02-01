53707bba5e311558d81f5dc0c4c2dcce0748e6a0
chaseknis <chase6th@gmail.com>
2023-02-01 11:46:16 +0200

    update webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, './src/index.js'),
   devServer: {
     static: './dist',
   },
